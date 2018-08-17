87fe6f492dfa412a30c7d1c87aae2c086a23743c
ramyaranjansahoo <42238970+ramyaranjansahoo@users.noreply.github.com>
2018-08-17 12:39:51 +0530

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,20 @@
+// library
+#include<iostream>
+using namespace std;
+int main()
+//process
+{
+//declaring variables
+int a=2;
+char b='3';
+float c=4.123;
+double d=5.24;
+bool e=1;
+//will get size of variables
+cout <<"size of int: "<<sizeof(int)<<"byte,"<<" contents of a= "<<a<<endl;
+cout <<"size of char: "<<sizeof(char)<<"byte,"<<" contents of b= "<<b<<endl;
+cout <<"size of float: "<<sizeof(float)<<"byte,"<<" contents of c= "<<c<<endl;
+cout <<"size of double: "<<sizeof(double)<<"byte,"<<" contents of d= "<<d<<endl;
+cout <<"size of bool: "<<sizeof(bool)<<"byte,"<<" contents of e= "<<e<<endl;
+return 1;
+}
---
+++ lab3_q2.cpp
@@ -0,0 +1,19 @@
+//Library
+#include<iostream>
+using namespace std;
+int main (){
+int a=90;
+int b=20;
+int sum = a+b;
+int difference= a-b;
+int multiply= a*b;
+int divide=a/b;
+int mod = a%b;
+cout<<"sum of a and b is " << sum<<endl;
+cout<<"difference of a and b is " <<difference<<endl;
+cout<<"mulltiplication of a and b is " <<multiply<<endl;
+cout<<"division of a and b is " <<divide<<endl;
+cout << "mod of 90 and 20 is " << mod << endl; 
+return 0;
+}
+
